928ea2a41e48373bbc1af87df521c10ec3c40f3d
Hiroya Furusawa <pogbba.6@gmail.com>
2022-09-20 01:38:45 +0900

    Number型を追加

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 "use strict";
+
+/*
+* Number型
+*/
+
+// 2進数
+console.log(0b101) // 5
+
+// 8進数
+console.log(0o211) // 137
+
+// 16進数
+console.log(0xFF) // 255
+
+// exponent（指数）
+console.log(3e4) // 30000 = 3x10*4
