e3d3a54a7412a779a78070acc2fa4157ef1b92f9
Hiroya Furusawa <pogbba.6@gmail.com>
2022-11-09 02:24:45 +0900

    Object.freeze()メソッドのセクションを追加

--- src/section7_object/index.js
+++ src/section7_object/index.js
@@ -1,3 +1,14 @@
 "use strict";
 
 // オブジェクト
+
+
+/*
+* Object.freeze() メソッド
+* ・変更不可のオブジェクトになる
+*/
+const obj = {
+  key: "value"
+}
+Object.freeze(obj);
+obj.key = "value2";  // Uncaught TypeError: Cannot assign to read only property 'key' of object '#<Object>'
