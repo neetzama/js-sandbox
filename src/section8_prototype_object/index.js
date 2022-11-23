06687fe51afe142e8a2a59cc2345533ea9dbdc0a
Hiroya Furusawa <pogbba.6@gmail.com>
2022-11-23 20:07:30 +0900

    プロトタイプメソッドのセクションを追加

--- src/section8_prototype_object/index.js
+++ src/section8_prototype_object/index.js
@@ -1,3 +1,28 @@
 "use strict";
 
 // プロトタイプオブジェクト
+
+/*
+* Object のインスタンスは自動的に prototype という特殊なプロパティが追加されている。prototype は Object.prototype を継承している。
+* Object.prototype　オブジェクトにはメソッドが定義されており、プロトタイプメソッドと呼ばれる。以下のようなプロトタイプメソッドが存在する。
+* ・toString()
+* ・valueOf()
+* ・hasOwnProperty() ...etc
+*
+*/
+
+const obj = {
+  key: "value"
+}
+console.log(obj.toString() === Object.prototype.toString())  // true
+
+/*
+* プロトタイプメソッドとインスタンスメソッドの優先順位
+* ・インスタンスメソッドが優先される
+*/
+const obj2 = {
+  toString() {
+    return "インスタンスメソッドが優先される"
+  }
+}
+console.log(obj2.toString())  // "インスタンスメソッドが優先される"
