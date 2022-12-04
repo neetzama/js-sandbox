cd2f5b86889670fe4985d89f6e1025cd9806e486
Hiroya Furusawa <pogbba.6@gmail.com>
2022-12-05 02:13:47 +0900

    prototype.atメソッドのセクションを追加

--- src/section9_array/index.js
+++ src/section9_array/index.js
@@ -1,3 +1,26 @@
 "use strict";
 
 // 配列
+
+
+/*
+* 疎な配列
+* ・未定義な箇所がある配列のこと
+* ・length としてはカウントされる
+*/
+const array = [1, , 3];
+console.log(array.length); // 3
+console.log(array[1]); // undefined
+
+
+/*
+* Array.prototype.at メソッド
+* ・相対的なインデックスの値を取得できる
+* ・末尾から数えた値を取得可能。array2[array2.length -1] と記述する必要がなくなる。
+*/
+const array2 = ["a", "b", "c", "d", "e"];
+console.log(array2.at(0));  // "a"
+console.log(array2.at(1));  // "b"
+
+console.log(array2.at(-1));  // "e"
+console.log(array2.at(-2));  // "d"
