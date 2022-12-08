174383953824371bad0f4904f0ff0d6635a97d24
Hiroya Furusawa <pogbba.6@gmail.com>
2022-12-09 02:30:04 +0900

    undefined と未定義の要素の違いについて

--- src/section9_array/index.js
+++ src/section9_array/index.js
@@ -14,6 +14,21 @@ console.log(array[1]); // undefined
 
 
 /*
+* undefined と未定義の要素の違い
+* ・Object.hasOwn() を使用し判定する
+*/
+// どちらも undefined と判定されてしまう
+const denseArray = [1, undefined, 3];
+const spaceArray = [1, , 3];
+console.log("denseArray", denseArray[1])  // undefined
+console.log("spaceArray", spaceArray[1])  // undefined
+
+// Object.hasOwn() を使用。第２引数には index を指定する
+console.log("denseArray", Object.hasOwn(denseArray, 1));  // true
+console.log("spaceArray", Object.hasOwn(spaceArray, 1));  // false
+
+
+/*
 * Array.prototype.at メソッド
 * ・相対的なインデックスの値を取得できる
 * ・末尾から数えた値を取得可能。array2[array2.length -1] と記述する必要がなくなる。
