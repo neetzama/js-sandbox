7936c80b396447f13e0838128f54424941a27e36
Hiroya Furusawa <pogbba.6@gmail.com>
2023-01-11 01:53:12 +0900

    グローバルスコープのセクションを追加

--- src/section_function_scope/index.js
+++ src/section_function_scope/index.js
@@ -21,3 +21,16 @@ const x = "global";
   console.log(x); // "outer"
 }
 console.log(x); // "global"
+
+
+/*
+* グローバルスコープ
+* ・グローバルスコープには自分が定義した変数の他にデフォルトでビルドインオブジェクトが宣言されている。以下2つがビルドインオブジェクトにあたる。
+* 　・ECMAScript の Array や RegExp などのコンストラクタ関数や undefined などの変数、isNaN のような関数など。
+* 　・実行環境（ブラウザや Node.js など）に定義されている document や module。
+* ・自分が定義した変数名とビルドインオブジェクトが被った場合、自分が定義した変数が優先される。そのため、以下のようなエラーが発生する。
+*/
+const Array = "stringArray";
+{
+  console.log(Array.from([1, 2, 3])) // Uncaught TypeError: Array.from is not a function
+}
