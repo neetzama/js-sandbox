b51a6ea5ace88bedd5dfc2420f33eba2d2759062
Hiroya Furusawa <pogbba.6@gmail.com>
2023-01-11 02:42:00 +0900

    varの巻き上げについてのセクションを追加

--- src/section_function_scope/index.js
+++ src/section_function_scope/index.js
@@ -32,5 +32,67 @@ console.log(x); // "global"
 */
 const Array = "stringArray";
 {
-  console.log(Array.from([1, 2, 3])) // Uncaught TypeError: Array.from is not a function
+  // console.log(Array.from([1, 2, 3])) // Uncaught TypeError: Array.from is not a function
 }
+
+/*
+* 関数スコープとvarの巻き上げ
+* ・var の巻き上げを理解するには「宣言」と「代入」から構成されると考える
+* 　・宣言は近い関数、またはグローバルスコープの先頭に巻き上げられる
+* 　・代入は部分はそのままの位置に残る
+* ・let, const は巻き上げが起きないため、読み込んだ時点でエラー（ReferenceError）を吐く。
+*/
+// let の場合
+// console.log(x3); // Uncaught ReferenceError: Cannot access 'x3' before initialization
+let x3 = "let_x3";
+
+/*
+* 1. グローバルスコープ
+*/
+console.log(x1); // undefined
+var x1 = "var_x1";
+
+// 上記は以下と同様のことを行っている。
+// スコープの先頭に宣言が巻き上げられる
+var x2;
+console.log(x2); // undefined
+// 代入部分がそのままの位置に残る
+x2 = "var_x2";
+
+
+/*
+* 2. 関数の先頭に巻き上げられた場合
+*/
+var x5 = 100;
+function a(){
+  console.log( x5 ); // undefined
+
+  var x5 = 200;
+  console.log( x5 ); // 200
+}
+a();
+
+// 上記は以下と同様のことを行っている。
+var x6 = 100;
+function a2(){
+  // 最も近い関数の先頭に宣言が巻き上げられる
+  var x6;
+  console.log( x6 ); // undefined
+
+  var x6 = 200;
+  console.log( x6 ); // 200
+}
+a2();
+
+/*
+* 3. 変数の巻き上げはブロックスコープを無視した場合
+*/
+function fn() {
+  // 内側のスコープを参照できる
+  console.log(x4); // => undefined
+  {
+    var x4 = "varのx4";
+  }
+  console.log(x4); // => "varのx"
+}
+fn();
